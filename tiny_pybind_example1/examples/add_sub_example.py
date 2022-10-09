408683e85e6e05803da9c2614e24213c118f796f
atinfinity <dandelion1124@gmail.com>
2022-10-09 11:55:32 +0900

    refactoring

--- tiny_pybind_example1/examples/add_sub_example.py
+++ tiny_pybind_example1/examples/add_sub_example.py
@@ -5,4 +5,3 @@ print(f"result1 = {result1}")
 
 result2 = tiny_pybind_example1.sub(1, 2)
 print(f"result2 = {result2}")
-
--- tiny_pybind_example2/examples/link_self_lib_example.py
+++ tiny_pybind_example2/examples/link_self_lib_example.py
@@ -1,4 +1,3 @@
 import tiny_pybind_example2
 
 tiny_pybind_example2.call()
-
