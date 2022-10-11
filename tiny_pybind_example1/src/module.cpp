aba01782f0a2b59601ef1208bbb77e9d98244176
atinfinity <dandelion1124@gmail.com>
2022-10-12 01:37:58 +0900

    minor changes

--- tiny_pybind_example1/src/module.cpp
+++ tiny_pybind_example1/src/module.cpp
@@ -3,11 +3,11 @@
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
+namespace py = pybind11;
+
 int add(int i, int j) {
     return i + j;
 }
-
-namespace py = pybind11;
 
 PYBIND11_MODULE(tiny_pybind_example1, m) {
     m.doc() = "tiny pybind example module";
